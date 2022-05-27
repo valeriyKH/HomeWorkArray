abb59aa064db78b41a512eace70fd397d82af10d
valera91k@yandex.ru <valera91k@yandex.ru>
2022-05-27 17:01:55 +0300

    First commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,17 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
-}
+  return arr1.length == arr2.length && arr1.every((v,i)=>v === arr2[i])
+};
 
 function advancedFilter(arr) {
   let resultArr;
+
+  resultArr = arr
+  .filter((arr) => arr % 3 === 0 && arr > 0) .map((arr) => arr * 10);
+  console.log(resultArr);
 
   // Ваш код
 
   return resultArr; // array
 }
+
